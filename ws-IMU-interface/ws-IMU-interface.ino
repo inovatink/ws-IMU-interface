c2604a4462db3ae4b66e56bcfc492bd35d8ab205
makefitter <31683512+makefitter@users.noreply.github.com>
2018-01-31 14:10:52 +0300

    Add files via upload

---
+++ ws-IMU-interface/ws-IMU-interface.ino
@@ -0,0 +1,225 @@
+#include <SparkFunMPU9250-DMP.h>                    // MPU-9250 Digital Motion Processing (DMP) Library
+#include "config.h"                                 // config.h is used to adjust specific parameters of the IMU
+
+MPU9250_DMP imu;                                    // Create an instance of the MPU9250_DMP class
+
+/* Logging Control Globals (uses definition from config.h*/
+
+bool enableSerialLogging = ENABLE_UART_LOGGING;
+bool enableTimeLog = ENABLE_TIME_LOG;
+bool enableCalculatedValues = ENABLE_CALCULATED_LOG;
+bool enableAccel = ENABLE_ACCEL_LOG;
+bool enableGyro = ENABLE_GYRO_LOG;
+bool enableCompass = ENABLE_MAG_LOG;
+bool enableQuat = ENABLE_QUAT_LOG;
+bool enableEuler = ENABLE_EULER_LOG;
+bool enableHeading = ENABLE_HEADING_LOG;
+unsigned short accelFSR = IMU_ACCEL_FSR;
+unsigned short gyroFSR = IMU_GYRO_FSR;
+unsigned short fifoRate = DMP_SAMPLE_RATE;
+
+
+/* LED Blink Control */
+
+uint32_t lastBlink = 0;
+
+void blinkLED()
+{
+  static bool ledState = false;
+  digitalWrite(HW_LED_PIN, ledState);
+  ledState = !ledState;
+}
+
+
+void setup()
+{
+  // Set up LED pin (active-high, default to off)
+  pinMode(HW_LED_PIN, OUTPUT);
+  digitalWrite(HW_LED_PIN, LOW);
+
+  // Set up MPU-9250 interrupt input (active-low)
+  pinMode(MPU9250_INT_PIN, INPUT_PULLUP);
+
+  // Set up serial log port
+  LOG_PORT.begin(SERIAL_BAUD_RATE);
+  
+  /* Initialize the MPU-9250. Should return true on success: */
+  if ( !initIMU() ) 
+  {
+    LOG_PORT.println("Error connecting to MPU-9250");
+    while (1) ;                                               // Loop forever if we fail to connect. LED will remain off in this state.
+  }
+}
+
+void loop()
+{
+  
+  // Then check IMU for new data, and log it
+  if ( !imu.fifoAvailable() ) // If no new data is available
+    return;                   // return to the top of the loop
+
+  // Read from the digital motion processor's FIFO
+  if ( imu.dmpUpdateFifo() != INV_SUCCESS )
+    return; // If that fails (uh, oh), return to top
+
+  // If enabled, read from the compass.
+  if ( (enableCompass || enableHeading) && (imu.updateCompass() != INV_SUCCESS) )
+    return; // If compass read fails (uh, oh) return to top
+
+  // If logging (to either UART and SD card) is enabled
+  if ( enableSerialLogging)
+    logIMUData(); // Log new data
+}
+
+void logIMUData(void)
+{
+  String imuLog = ""; // Create a fresh line to log
+  if (enableTimeLog) // If time logging is enabled
+  {
+    imuLog += String(imu.time) + ", "; // Add time to log string
+  }
+  
+  if (enableAccel) // If accelerometer logging is enabled
+  {
+    if ( enableCalculatedValues ) // If in calculated mode
+    {
+      imuLog += String(imu.calcAccel(imu.ax)) + ", ";
+      imuLog += String(imu.calcAccel(imu.ay)) + ", ";
+      imuLog += String(imu.calcAccel(imu.az)) + ", ";
+    }
+    else
+    {
+      imuLog += String(imu.ax) + ", ";
+      imuLog += String(imu.ay) + ", ";
+      imuLog += String(imu.az) + ", ";
+    }
+  }
+  
+  if (enableGyro) // If gyroscope logging is enabled
+  {
+    if ( enableCalculatedValues ) // If in calculated mode
+    {
+      imuLog += String(imu.calcGyro(imu.gx)) + ", ";
+      imuLog += String(imu.calcGyro(imu.gy)) + ", ";
+      imuLog += String(imu.calcGyro(imu.gz)) + ", ";
+    }
+    else
+    {
+      imuLog += String(imu.gx) + ", ";
+      imuLog += String(imu.gy) + ", ";
+      imuLog += String(imu.gz) + ", ";
+    }
+  }
+  
+  if (enableCompass) // If magnetometer logging is enabled
+  {
+    if ( enableCalculatedValues ) // If in calculated mode
+    {
+      imuLog += String(imu.calcMag(imu.mx)) + ", ";
+      imuLog += String(imu.calcMag(imu.my)) + ", ";
+      imuLog += String(imu.calcMag(imu.mz)) + ", ";    
+    }
+    else
+    {
+      imuLog += String(imu.mx) + ", ";
+      imuLog += String(imu.my) + ", ";
+      imuLog += String(imu.mz) + ", ";
+    }
+  }
+  
+  if (enableQuat) // If quaternion logging is enabled
+  {
+    if ( enableCalculatedValues )
+    {
+      imuLog += String(imu.calcQuat(imu.qw), 4) + ", ";
+      imuLog += String(imu.calcQuat(imu.qx), 4) + ", ";
+      imuLog += String(imu.calcQuat(imu.qy), 4) + ", ";
+      imuLog += String(imu.calcQuat(imu.qz), 4) + ", ";
+    }
+    else
+    {
+      imuLog += String(imu.qw) + ", ";
+      imuLog += String(imu.qx) + ", ";
+      imuLog += String(imu.qy) + ", ";
+      imuLog += String(imu.qz) + ", ";      
+    }
+  }
+  
+  if (enableEuler) // If Euler-angle logging is enabled
+  {
+    imu.computeEulerAngles();
+    imuLog += String(imu.pitch, 2) + ", ";
+    imuLog += String(imu.roll, 2) + ", ";
+    imuLog += String(imu.yaw, 2) + ", ";
+  }
+  
+  if (enableHeading) // If heading logging is enabled
+  {
+    imuLog += String(imu.computeCompassHeading(), 2) + ", ";
+  }
+  
+  // Remove last comma/space:
+  imuLog.remove(imuLog.length() - 2, 2);
+  imuLog += "\r\n"; // Add a new line
+
+  if (enableSerialLogging)  // If serial port logging is enabled
+  LOG_PORT.print(imuLog); // Print log line to serial port
+
+  // Blink LED once every second (if only logging to serial port)
+    if ( millis() > lastBlink + UART_BLINK_RATE )
+    {
+      blinkLED(); 
+      lastBlink = millis();
+    }
+}
+
+
+
+bool initIMU(void)
+{
+  // imu.begin() should return 0 on success. Will initialize
+  // I2C bus, and reset MPU-9250 to defaults.
+  if (imu.begin() != INV_SUCCESS)
+    return false;
+
+  // Set up MPU-9250 interrupt:
+  imu.enableInterrupt(); // Enable interrupt output
+  imu.setIntLevel(1);    // Set interrupt to active-low
+  imu.setIntLatched(1);  // Latch interrupt output
+
+  // Configure sensors:
+  // Set gyro full-scale range: options are 250, 500, 1000, or 2000:
+  imu.setGyroFSR(gyroFSR);
+  // Set accel full-scale range: options are 2, 4, 8, or 16 g 
+  imu.setAccelFSR(accelFSR);
+  // Set gyro/accel LPF: options are5, 10, 20, 42, 98, 188 Hz
+  imu.setLPF(IMU_AG_LPF); 
+  // Set gyro/accel sample rate: must be between 4-1000Hz
+  // (note: this value will be overridden by the DMP sample rate)
+  imu.setSampleRate(IMU_AG_SAMPLE_RATE); 
+  // Set compass sample rate: between 4-100Hz
+  imu.setCompassSampleRate(IMU_COMPASS_SAMPLE_RATE); 
+
+  // Configure digital motion processor. Use the FIFO to get
+  // data from the DMP.
+  unsigned short dmpFeatureMask = 0;
+  if (ENABLE_GYRO_CALIBRATION)
+  {
+    // Gyro calibration re-calibrates the gyro after a set amount
+    // of no motion detected
+    dmpFeatureMask |= DMP_FEATURE_SEND_CAL_GYRO;
+  }
+  else
+  {
+    // Otherwise add raw gyro readings to the DMP
+    dmpFeatureMask |= DMP_FEATURE_SEND_RAW_GYRO;
+  }
+  // Add accel and quaternion's to the DMP
+  dmpFeatureMask |= DMP_FEATURE_SEND_RAW_ACCEL;
+  dmpFeatureMask |= DMP_FEATURE_6X_LP_QUAT;
+
+  // Initialize the DMP, and set the FIFO's update rate:
+  imu.dmpBegin(dmpFeatureMask, fifoRate);
+
+  return true; // Return success
+}
